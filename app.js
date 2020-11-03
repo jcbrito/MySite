47936fbe1620bd64a4495f6d867081fe8f145ced
jbrito <jcbritov2@gmail.com>
2020-11-03 11:06:03 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,11 +1,15 @@
 var express = require('express');
 var app = express();
+var fs = require('fs');
 
 const port = process.env.PORT || 8080
 
-// app.get('/', function (req, res) {
-//   res.send('Hello World!');
-// });
+app.get('/', function (req, res) {
+  fs.readFile('index.html', function(err, data) {
+    res.writeHead(200, {'Content-Type': 'text/html'});
+    res.write(data);
+  res.send();
+});
 
 app.listen(port, function () {
   console.log('Example app listening on port 3000!');
